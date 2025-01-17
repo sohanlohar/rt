894755ce4252d0c60bf0adb0990d0b9ef1f2c32b
sohanlohar <loharsonu11@gmail.com>
2025-01-17 17:38:31 +0530

    Commit changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "export", // Enable static export
-  basePath: "/rt", // Replace with your GitHub repository name
-  assetPrefix: "/rt/",
+  output: "export",
 };
 
 export default nextConfig;
