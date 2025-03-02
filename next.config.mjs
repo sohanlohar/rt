77f13dc3e2d0c3d92c8f565049ff550f86cc67cb
sohanlohar <loharsonu11@gmail.com>
2025-03-03 01:57:38 +0530

    issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enables static export
+  output: "export", 
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Required for images on GitHub Pages
+    unoptimized: true, 
   },
-  basePath: "https://github.com/sohanlohar/rt", // Set this to your GitHub repository name
-  assetPrefix: "https://github.com/sohanlohar/rt",
 };
 
 export default nextConfig;
